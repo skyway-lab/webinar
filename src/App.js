334832dacc70f3708358b5a847d7ba23ce420659
Ryosuke Otsuya <r.otsuya@gmail.com>
2016-09-08 12:47:49 +0900

    create components for a speaker and listeners

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <SpeakerUi></SpeakerUi>
+                <ListenerUi></ListenerUi>
+            </div>
+        );
+    }
+}
+
+class SpeakerUi extends Component {
+    render () {
+        return (
+            <div>
+                <h1>講師</h1>
+            </div>
+        );
+    }
+}
+
+class ListenerUi extends Component {
+    render () {
+        return (
+            <div>
+                <h1>視聴者</h1>
+            </div>
+        );
+    }
 }
 
 export default App;
