77b92d57900d3828472e1f65645298beaf763810
Ryosuke Otsuya <r.otsuya@gmail.com>
2016-11-27 08:45:33 +0900

    On going

--- src/getCameraStream.js
+++ src/getCameraStream.js
@@ -5,20 +5,22 @@ import CONST from './Const';
 // this props alerts
 
 function getCameraStream(width, height, frameRate, isMuted, callback, videoInId = this.props.videoInId, audioInId = this.props.audioInId) {
-    const videoConstraints = {
-        deviceId: videoInId ? { exact: videoInId } :undefined,
-        width,
-        height,
-        frameRate
-    };
-    const audioConstraints = {
-        deviceId: audioInId ? { exact: audioInId } :undefined,
+    const constraints = {
+        video: {
+            deviceId: videoInId ? { exact: videoInId } :undefined,
+            width,
+            height,
+            frameRate
+        },
+        audio: {
+            deviceId: audioInId ? { exact: audioInId } :undefined,
+        }
     };
     const timerAlertGUM = setTimeout(() => {
         this.props.update([{ op: 'add', path: '/alerts/-', value: CONST.ALERT_KIND_GUM }]);
     }, CONST.TIMEOUT_MILLISECONDS_ALERT_GUM);
-    console.info('constraints', {audio: audioConstraints, video: videoConstraints});
-    navigator.mediaDevices.getUserMedia({ audio: audioConstraints, video: videoConstraints })
+    console.info('constraints', constraints);
+    navigator.mediaDevices.getUserMedia(constraints)
         .then(cameraStream => {
             const index = this.props.alerts.indexOf(CONST.ALERT_KIND_GUM);
             if (index !== -1) {
@@ -41,9 +43,9 @@ function getCameraStream(width, height, frameRate, isMuted, callback, videoInId 
                 callback(cameraStream);
             }
         }).catch(err => {
-        console.error(err);
-        this.props.update([{ op: 'add', path: '/alerts/-', value: CONST.ALERT_KIND_GUM }]);
-    });
+            console.error(err);
+            this.props.update([{ op: 'add', path: '/alerts/-', value: CONST.ALERT_KIND_GUM }]);
+        });
 }
 
 export default getCameraStream;
